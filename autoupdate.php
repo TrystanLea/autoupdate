b4b6af7aa3bfd19ec06f5a052a337e165588a238
Trystan Lea <trystan.lea@googlemail.com>
2017-08-06 15:20:15 +0100

    Update autoupdate.php

--- autoupdate.php
+++ autoupdate.php
@@ -2,4 +2,4 @@
 
 include "settings.php";
 
-file_get_contents("https://emoncms.org/input/post?json={autoupdate:123}&apikey=$apikey");
+file_get_contents("https://emoncms.org/input/post?json={autoupdate:1234}&apikey=$apikey");
